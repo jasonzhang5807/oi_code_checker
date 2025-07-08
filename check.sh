1b12b28bf33f02fa3f72761b542d6f9e1e3a7f09
jasonzhang5807 <106912832+jasonzhang5807@users.noreply.github.com>
2025-07-08 12:38:14 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # This script is used to check user's oi program and the correct program.
 if [ $# -eq 3 ]; then
-    $1 > /tmp/input.txt
-    $2 < /tmp/input.txt > /tmp/user_output.txt
-    $3 < /tmp/input.txt > /tmp/correct_output.txt
+    $1 > /tmp/data.txt
+    $2 < /tmp/data.txt > /tmp/user_output.txt
+    $3 < /tmp/data.txt > /tmp/correct_output.txt
     diff /tmp/user_output.txt /tmp/correct_output.txt
     if [ $? -eq 0 ]; then
         echo "Output matches!"
